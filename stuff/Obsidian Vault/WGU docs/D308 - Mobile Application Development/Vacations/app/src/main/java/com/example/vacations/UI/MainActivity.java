216216bdcb19931c2f19f1fd6b3189bf231adfff
Ben Lyakhov <blyakho@wgu.edu>
2025-11-17 19:27:58 -0800

    1. Created UI and dao packages 2. AndroidManifest.xml: added labels for ProductList and MainActivity (lines 16 and 20) 3. added ProductDetails class

---
+++ stuff/Obsidian Vault/WGU docs/D308 - Mobile Application Development/Vacations/app/src/main/java/com/example/vacations/UI/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.vacations.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vacations.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button); //button not defined here, but is defined in the video
+        // her MainActivity.Java file is under her UI folder, which I don't have/didn't create
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ProductList.class);
+                startActivity(intent);
+            }
+        });
+
+// ------ the below code is missing from the panopto video part 1. ------------------------
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
---
+++ stuff/Obsidian Vault/WGU docs/D308 - Mobile Application Development/Vacations/app/src/main/java/com/example/vacations/UI/ProductDetails.java
@@ -0,0 +1,4 @@
+package com.example.vacations.UI;
+
+public class ProductDetails {
+}
