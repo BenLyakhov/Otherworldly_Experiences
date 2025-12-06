47c0949d68ffc4f0610ee9b75ee4d39576d6b2b7
Ben Lyakhov <blyakho@wgu.edu>
2025-12-05 17:49:03 -0800

    Created local repository for this project. Will test pushing it to Gitlab after.

---
+++ app/src/main/java/com/example/vacations/UI/ProductDetails.java
@@ -0,0 +1,28 @@
+package com.example.vacations.UI;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.vacations.R;
+
+public class ProductDetails extends AppCompatActivity {
+
+//    When adding any new empty view activity to the UI folder, don't forget to include the label (name) in the AndroidManifest.xml file
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_product_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
