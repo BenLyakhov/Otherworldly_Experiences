728c5fdf7ed6013747d42e30edcc67367117e52d
Ben Lyakhov <blyakho@wgu.edu>
2025-12-16 18:50:00 -0800

    added several files, following along with video 3 of Panopto recordings: 1. VacationAdapter 2. vacation_list_item.xml 3. excursion_list_item.xml
    modified activity_vacation_details.xml and activity_vacation_list.xml.
    
    Stopped at 48:17 in video 3

---
+++ stuff/Obsidian Vault/WGU docs/D308 - Mobile Application Development/Vacations/app/src/main/java/com/example/vacations/UI/VacationAdapter.java
@@ -0,0 +1,77 @@
+package com.example.vacations.UI;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vacations.R;
+import com.example.vacations.entities.Vacation;
+
+import java.util.List;
+
+public class VacationAdapter extends RecyclerView.Adapter<VacationAdapter.VacationViewHolder> {
+//    mVacations is needed for the method on line 55, setVacations.
+//    this method is needed for getAbsoluteAdapterPosition(), line 32
+    private List<Vacation>  mVacations;
+    private final Context context;
+    private final LayoutInflater mInflater;
+    public VacationAdapter(Context context){
+        mInflater= LayoutInflater.from(context);
+        this.context=context;
+    }
+    public class VacationViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView vacationItemView;
+
+        public VacationViewHolder(@NonNull View itemView) {
+            super(itemView);
+            vacationItemView=itemView.findViewById(R.id.textView4); //textView4 is the text view ID in vacation_list_item.xml
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//              Panopto video 3 has getAdapterPosition(). This is depreciated in my current version of Android studio.
+//              I will try instead to use what was suggested by the IDE, getAbsoluteAdapterPosition();
+//              if this doesn't work, try the other suggestion, getBindingAdapterPosition();
+                    int position=getAbsoluteAdapterPosition();
+                    final Vacation current=mVacations.get(position);
+                    Intent intent=new Intent(context,VacationDetails.class);
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public VacationAdapter.VacationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView=mInflater.inflate(R.layout.vacation_list_item,parent,false);
+        return new VacationViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VacationAdapter.VacationViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+//        return 0; // dangerous
+//        instead want to return the number of products (vacations) you have.
+        if (mVacations!=null){
+            return mVacations.size();
+        } else return 0;
+    }
+
+//    need the following method for the getAbsoluteAdapterPosition(), line 27
+    public void setVacations(List<Vacation> vacations){
+        mVacations=vacations;
+        notifyDataSetChanged();
+    }
+
+}
