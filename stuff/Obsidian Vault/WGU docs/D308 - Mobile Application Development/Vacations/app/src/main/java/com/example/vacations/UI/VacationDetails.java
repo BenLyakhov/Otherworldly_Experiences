d19a91b999db3554059ebd37e991dbff0f75d08a
Ben Lyakhov <blyakho@wgu.edu>
2026-01-22 20:01:11 -0800

    ended at 1:36:05, video 3. added menu_excursiondetails.xml for saving and deleting vacations
    dded lines 70 to 92 in VacationDetails.java to edit or add vacations, the app works, just have to back out to home screen to see added vacation (testing with Japan, 500.0, showed up in database as well.)

--- stuff/Obsidian Vault/WGU docs/D308 - Mobile Application Development/Vacations/app/src/main/java/com/example/vacations/UI/VacationDetails.java
+++ stuff/Obsidian Vault/WGU docs/D308 - Mobile Application Development/Vacations/app/src/main/java/com/example/vacations/UI/VacationDetails.java
@@ -2,6 +2,8 @@ package com.example.vacations.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vacations.R;
 import com.example.vacations.database.Repository;
+import com.example.vacations.entities.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationDetails extends AppCompatActivity {
@@ -41,6 +44,7 @@ public class VacationDetails extends AppCompatActivity {
 
             editName=findViewById(R.id.titleText);
             editPrice=findViewById(R.id.priceText);
+            vacationID = getIntent().getIntExtra("id", -1);
             name = getIntent().getStringExtra("name");
             price = getIntent().getDoubleExtra("price",0.0);
             editName.setText(name);
@@ -63,4 +67,30 @@ public class VacationDetails extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         excursionAdapter.setExcursions(repository.getmAllExcursions());
         }
+
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.menu_excursiondetails, menu);
+            return true;
+        }
+
+        public boolean onOptionsItemSelected(MenuItem item) {
+            if(item.getItemId()==R.id.vacationsave){
+                Vacation vacation;
+                if (vacationID==-1){
+                    if (repository.getmAllVacations().size() == 0) vacationID = 1;
+                    else vacationID = repository.getmAllVacations().get(repository.getmAllVacations().size() - 1).getVacationID() +1;
+//          The above else statement is saying if the repository (ie, list of all vacations) is not 0, get the list, count its size, -1
+//          because coding is 0 based, and then add one to insert the new vacation at the end of the list
+                    vacation = new Vacation(vacationID, editName.getText().toString(), Double.parseDouble((editPrice.getText().toString())));
+                    repository.insert(vacation);
+                }
+                else { // this else statement is for if you are modifying a vacation (time stamp video 3, 1:29:40)
+                    vacation = new Vacation(vacationID, editName.getText().toString(), Double.parseDouble((editPrice.getText().toString())));
+                    repository.update(vacation);
+                }
+            }
+            return true;
+        }
+
     }
+
